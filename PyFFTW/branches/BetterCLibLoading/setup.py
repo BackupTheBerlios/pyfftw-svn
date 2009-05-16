b33fd5220484bd9385747431f8146c8f32228116
jschrod <jschrod@46e99ce5-d1e1-4c1a-9b0b-b76595b0e82e>
2009-05-16 10:39:57 +0000

    PyFFTW: created branch which allows to specify the location of fftw library at build time

---
+++ PyFFTW/branches/BetterCLibLoading/setup.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from distutils.log import warn
+from distutils.command.build_py import build_py
+import ctypes
+from ctypes import util
+import os
+
+FFTW_PATH = '/usr/lib/'
+packages_library_names = {'fftw3': 'libfftw3.so', 'fftw3f' : 'libfftw3f.so',
+                          'fftw3l': 'libfftw3l.so'}
+
+_complex_typedict = {'fftw3':'complex', 'fftw3f': 'singlecomplex', 'fftw3l': 'longcomplex'}
+_float_typedict = {'fftw3': 'double', 'fftw3f': 'single', 'fftw3l': 'longdouble'}
+packages_0 = ['fftw3','fftw3f', 'fftw3l']
+
+# To used threads, p+'_threads' library must exist in the system where p in packages_0.
+
+def create_source_from_template(tmplfile, outfile, lib, libname, _complex,
+                                _float, location):
+    print location
+    fp = open(tmplfile, 'r')
+    tmpl = fp.read()
+    fp.close()
+    mod = tmpl.replace('$libname$',libname).replace('$complex$',_complex).\
+            replace('$library$',lib).replace('$float$',_float).\
+            replace('$libraryfullpath$', location)
+    fp = open(outfile, 'w')
+    fp.write(mod)
+    fp.close()
+    print "build %s from template %s" %(outfile, tmplfile)
+
+def check_libs(packages):
+    libs = {}
+    for name in packages[:]:
+        try:
+            libpath = os.path.join(FFTW_PATH, packages_library_names[name])
+            lib = ctypes.cdll.LoadLibrary(libpath)
+        except OSError, e:
+            warn("%s is not located at %s trying util.find_library(%s)"
+                 %(name, libpath, name))
+            try:
+                libpath = util.find_library(name)
+                lib = ctypes.cdll.LoadLibrary(libpath)
+            except (TypeError,OSError), e:
+                warn("Not installing bindings for %s, because could not load\
+                     the library: %s\n if you know the library is installed\
+                     you can specify the absolute path in setup.py" % (name, e))
+                packages.remove(name)
+        libs[name] = libpath
+    return packages, libs
+
+packages, liblocation = check_libs(packages_0)
+
+class build_from_templates(build_py):
+    def build_module(self, module, module_file, package):
+        module, ext = os.path.splitext(module)
+        if not ext == '.tmpl':
+            outfile = self.get_module_outfile(self.build_lib, [package], module)
+            dir = os.path.dirname(outfile)
+            self.mkpath(dir)
+            return self.copy_file(module_file, outfile, preserve_mode=0)
+        else:
+            outfile = self.get_module_outfile(self.build_lib, [package], module)
+            dir = os.path.dirname(outfile)
+            self.mkpath(dir)
+            return create_source_from_template(module_file, outfile, package,
+                                               package.replace('3',''),
+                                               _complex_typedict[package],
+                                               _float_typedict[package],
+                                               liblocation[package])
+
+
+setup(name='PyFFTW3',
+      version='0.2',
+      description='Python bindings to the FFTW3 C-library',
+      long_description='PyFFTW provide bindings to access the FFTW3 C-library (http://www.fftw.org) from Python',
+      author='Jochen Schroeder',
+      author_email='jschrod@berlios.de',
+      url = 'pyfftw.berlios.de',
+      packages=packages,
+      package_dir = dict ([(n, 'src/templates') for n in packages]),
+      cmdclass = {"build_py": build_from_templates},
+      license ='GPL v3'
+     )
